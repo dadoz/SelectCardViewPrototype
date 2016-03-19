c44df5443dbd2b598dcfbb00c23f489333c78467
davide-ennova <davide.lamanna@ennova.it>
2016-03-19 13:20:31 +0100

    added flurry to trace access to app

--- app/src/main/java/com/application/sample/selectcardviewprototype/app/MainActivity.java
+++ app/src/main/java/com/application/sample/selectcardviewprototype/app/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.MenuItem;
 import com.application.sample.selectcardviewprototype.app.fragment.OnRestoreRecyclerViewInterface;
 import com.application.sample.selectcardviewprototype.app.fragment.ContactListFragment;
 import com.application.sample.selectcardviewprototype.app.singleton.StatusSingleton;
+import com.flurry.android.FlurryAgent;
 
 import static com.application.sample.selectcardviewprototype.app.singleton.StatusSingleton.*;
 import static com.application.sample.selectcardviewprototype.app.singleton.StatusSingleton.StatusEnum.*;
@@ -21,6 +22,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         status = getInstance();
+
+        // configure Flurry
+        FlurryAgent.setLogEnabled(false);
+        // init Flurry
+        FlurryAgent.init(this, getResources().getString(R.string.FLURRY_APIKEY));
+
         if (savedInstanceState == null) {
             getSupportFragmentManager()
                     .beginTransaction()
